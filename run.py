44eca4c836c578c317da6f5d0da651a3926dbc7d
alineg-di <alineg@deepinstinct.com>
2023-02-06 20:24:13 +0200

    first commit run app

--- run.py
+++ run.py
@@ -1,18 +1,17 @@
+import rook
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+
 from app import router
-import rook
 
 
-def main():
-    app = FastAPI()
-    app.include_router(router)
-    app.mount("/", StaticFiles(directory="static", html=True), name="static")
-    return app
+
+app = FastAPI()
+app.include_router(router)
+app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 
 rook.start(
-    token="XXXXXXXXXXXXXXXX",
-    labels={"env": "dev"},
-    fork=True
+    token='15fceae1c79b9fa8db7b7eb3f67fcfcd0184f7ec57c25b12701caf81c2df679a',
+    labels={"env": "dev"}
 )
